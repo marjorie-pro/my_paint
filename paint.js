cefced57248f7ba9ce458ee6898108c9a9c67845
marjo <pezeron.marjorie@gmail.com>
2019-11-01 12:40:40 +0100

    suite outils my_paint

--- paint.js
+++ paint.js
@@ -11,9 +11,13 @@
 
 var canvas=document.getElementById("canvas");
 var ctx=canvas.getContext("2d");
+ctx.fillStyle = "white";
+var cbg = ctx.fillStyle;
+// var strokeColor="red";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 var lastX;
 var lastY;
-var strokeColor="red";
+
 var strokeWidth=5;
 var mouseX;
 var mouseY;
@@ -68,6 +72,7 @@ function handleMouseMove(e){
     }else{
       ctx.globalCompositeOperation="destination-out";
       ctx.arc(lastX,lastY,8,0,Math.PI*2,false);
+      $("#canvas").css("background-color", cbg);
       ctx.fill();
     }
     lastX=mouseX;
@@ -122,7 +127,7 @@ $("#eraser").click(function(){ mode="eraser"; });
 ////////////////////////////////////////////////////////////////////:
 
 
-
+//LIGNE
 var mouse = {
     x: 0,
     y: 0
@@ -130,125 +135,134 @@ var mouse = {
 
 $("#line").click(function(){
     $("#canvas").css('cursor','crosshair');
-    $("canvas").click(function(e){
+    $("canvas").click(function(el){
         if(mouse.x != 0 && mouse.y != 0){
         	
             ctx.beginPath();
             ctx.moveTo(mouse.x, mouse.y);
-            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.lineTo(el.offsetX, el.offsetY);
             ctx.closePath();
             ctx.stroke();
             mouse.x = 0;
             mouse.y = 0;
         }else{
-            mouse.x = e.offsetX;
-            mouse.y = e.offsetY;
+            mouse.x = el.offsetX;
+            mouse.y = el.offsetY;
         }
     });
 });
 
-var mouseC = {
-    xC: 0,
-    yC: 0
-};
+//CERCLE
+// var mouseC = {
+//     xC: 0,
+//     yC: 0
+// };
 $("#circle").click(function(){
     $("#canvas").css('cursor','crosshair');
     $("canvas").click(function(e){
-        	
+        mouseX=parseInt(e.clientX-offsetX);
+        mouseY=parseInt(e.clientY-offsetY);
+        console.log(mouseX);	
         ctx.beginPath();
-        ctx.arc(100,100, 20, 0, 2 * Math.PI);
+        ctx.lineWidth = $("#epaisseurC").val();
+        ctx.arc(mouseX,mouseY, 20 , 0, 2 * Math.PI);
         ctx.closePath();
         ctx.stroke(); 
          
     });
 });
 
+//RECTANGLE VIDE
+
+var isDrawing = false;
+var startX;
+var startY;
+
+$("#rectangle").click(function(){
+$("#canvas").mousedown(function (e) {
+    recthandleMouseDown(e);
+});
+});
+function recthandleMouseDown(e) {
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
+    // $("#downlog").html("Down: " + mouseX + " / " + mouseY);
+
+    // Put your mousedown stuff here
+    if (isDrawing) {
+        isDrawing = false;
+        ctx.strokeStyle = $("#color_picker").val();
+        ctx.beginPath();
+        ctx.lineWidth = $("#epaisseurRV").val();
+        ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
+        ctx.closePath();
+        ctx.stroke(); 
+        canvas.style.cursor = "default";
+    } else {
+        isDrawing = true;
+        startX = mouseX;
+        startY = mouseY;
+        $("#canvas").css('cursor','crosshair');
+    }
+
+}
+//RECTANGLE REMPLIE
+
+var risDrawing = false;
+var rstartX;
+var rstartY;
+$("#rectangleR").click(function(){
+$("#canvas").mousedown(function (er) {
+    rectRhandleMouseDown(er);
+});
+});
+
+function rectRhandleMouseDown(er) {
+    rmouseX = parseInt(er.clientX - offsetX);
+    rmouseY = parseInt(er.clientY - offsetY);
+    // $("#downlog").html("Down: " + mouseX + " / " + mouseY);
+
+    // Put your mousedown stuff here
+    if (risDrawing) {
+        risDrawing = false;
+        ctx.fillStyle = $("#color_picker").val();
+        ctx.beginPath();
+        ctx.rect(rstartX, rstartY, rmouseX - rstartX, rmouseY - rstartY);
+        ctx.closePath();
+        ctx.fill(); 
+        canvas.style.cursor = "default";
+    } else {
+        risDrawing = true;
+        rstartX = rmouseX;
+        rstartY = rmouseY;
+        $("#canvas").css('cursor','crosshair');
+    }
+
+}
+
+//SAUVEGARDE
 var button = document.getElementById('btn-download');
 button.addEventListener('click', function (e) {
     var dataURL = canvas.toDataURL('image/png');
     button.href = dataURL;
 });
-
+//OUVRIR FICHIER
 $('#button').on('click', function() {
     $('#file-input').trigger('click');
 });
 
-// var canvas = new fabric.Canvas('c', { selection: false });
+//CLEAR
+document.getElementById('clear').addEventListener('click', function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }, false);
 
-// var circle, isDown, origX, origY;
+//BACK
+document.getElementById('cancel').addEventListener('click', function() {
 
-// canvas.on('mouse:down', function(o){
-//   isDown = true;
-//   var pointer = canvas.getPointer(o.e);
-//   origX = pointer.x;
-//   origY = pointer.y;
-//   circle = new fabric.Circle({
-//     left: pointer.x,
-//     top: pointer.y,
-//     radius: 1,
-//     strokeWidth: 5,
-//     stroke: 'red',
-//     selectable: false,
-//     originX: 'center', originY: 'center'
-//   });
-//   canvas.add(circle);
-// });
+      canvas.remove();
 
-// canvas.on('mouse:move', function(o){
-//   if (!isDown) return;
-//   var pointer = canvas.getPointer(o.e);
-//   circle.set({ radius: Math.abs(origX - pointer.x) });
-//   canvas.renderAll();
-// });
+      return false;
 
-// canvas.on('mouse:up', function(o){
-//   isDown = false;
-// });
-
-// $('#canvas').attr('height', $('#canvas').css('height'));
-//     $('#canvas').attr('width', $('#canvas').css('width'));
-    
-//     $("#canvas").click(function(e){ 
-
-//         var x = e.pageX - this.offsetLeft;
-//         var y = e.pageY - this.offsetTop; 
-
-//         /* var c=document.getElementById("special"); */
-//         var ctx= this.getContext("2d"); /*c.getContext("2d");*/
-//         ctx.beginPath();
-//         ctx.arc(x, y, 10,0, 2*Math.PI);
-//         ctx.stroke();
-
-//    });
-
-
-  // var letsdraw ;
-
-  // var canvasOffset = $('#canvas').offset();
-
-  // $('#canvas').mousemove(function(e) {
-  //   if (letsdraw) {
-  //     ctx.clearRect(0,0,canvas.width,canvas.height);
-  //     ctx.strokeStyle = 'blue';
-  //     ctx.lineWidth = 1;
-  //     ctx.beginPath();
-    
-  //     ctx.moveTo(letsdraw.x, letsdraw.y);
-  //     ctx.lineTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
-  //     ctx.stroke();
-  //   }
-  // });
-
-  // $('#canvas').mousedown(function(e) {
-  //   letsdraw = {
-  //     x:e.pageX - canvasOffset.left,
-  //     y:e.pageY - canvasOffset.top
-  //     }
-    
-  // });
-
-  // $(window).mouseup(function() {
-  //   letsdraw = null;
-  // });
+    });
 
 })(jQuery);
