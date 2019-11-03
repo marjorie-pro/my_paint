4e16141e8d5b0218a19d7660e817f1fd6f41e357
marjo <pezeron.marjorie@gmail.com>
2019-11-03 20:36:19 +0100

    final envoi

--- paint.js
+++ paint.js
@@ -1,12 +1,3 @@
-// var canvas = document.getElementById('canvas');
-// var ctx = canvas.getContext('2d');
-// ctx.fillStyle = 'black';
-// ctx.fillRect(30,30,730,400);
-
-// $(#crayon).click(function(){
-
-// });
-
 (function($){
 
 var canvas=document.getElementById("canvas");
@@ -33,14 +24,14 @@ var isMouseDown=false;
 $("label.btn").prop("checked",$(this).prop("checked"));
 
 // GÉNÉRER LES MENUS
-        $('#showtool').click(function() {
-                $('.menu_tool').slideToggle("fast");
-        });
-        $('#showform').click(function() {
-                $('.menu_form').slideToggle("fast");
-        });
+        // $('#showtool').click(function() {
+        //         $('.menu_tool').slideToggle("fast");
+        // });
+        // $('#showform').click(function() {
+        //         $('.menu_form').slideToggle("fast");
+        // });
 
-
+//CRAYON & GOMME
 function handleMouseDown(e){
   mouseX=parseInt(e.clientX-offsetX);
   mouseY=parseInt(e.clientY-offsetY);
@@ -55,7 +46,6 @@ function handleMouseUp(e){
   mouseX=parseInt(e.clientX-offsetX);
   mouseY=parseInt(e.clientY-offsetY);
 
-  // Put your mouseup stuff here
   isMouseDown=false;
 }
 
@@ -63,7 +53,6 @@ function handleMouseOut(e){
   mouseX=parseInt(e.clientX-offsetX);
   mouseY=parseInt(e.clientY-offsetY);
 
-  // Put your mouseOut stuff here
   isMouseDown=false;
 }
 
@@ -71,7 +60,6 @@ function handleMouseMove(e){
   mouseX=parseInt(e.clientX-offsetX);
   mouseY=parseInt(e.clientY-offsetY);
 
-  // Put your mousemove stuff here
   if(isMouseDown){
   	$("#canvas").css('cursor','pointer');
     ctx.beginPath();
@@ -86,6 +74,7 @@ function handleMouseMove(e){
     }else{
       ctx.globalCompositeOperation="destination-out";
       ctx.arc(lastX,lastY,8,0,Math.PI*2,false);
+      ctx.lineWidth = $("#epaisseur").val();
       $("#canvas").css("background-color", cbg);
       ctx.fill();
     }
@@ -104,54 +93,17 @@ $("#pen").click(function(){ mode="pen"; });
 $("#eraser").click(function(){ mode="eraser"; });
 
 
-////////////////////////////////////////////////////////////////
-
-
-// function lineMouse(e){
-// 	var mouse = {
-// 	x: -1,
-//     y: -1
-// };
-//  var cvs = $("canvas")[0].getContext("2d");
-//     $("canvas").click(function(e){
-//         if(mouse.x != -1 && mouse.y != -1){
-//         	$("#canvas").css('cursor','crosshair');
-//             ctx.beginPath();
-//             ctx.moveTo(mouse.x, mouse.y);
-//             ctx.lineTo(e.pageX, e.pageY);
-//             ctx.closePath();
-//             ctx.stroke();
-//             mouse.x = -1;
-//             mouse.y = -1;
-//         }else{
-//             mouse.x = e.pageX;
-//             mouse.y = e.pageY;
-//         }
-//     });
-
-// }
-
-
-// $("#line").click(function(){ 
-// 	mode="line";
-// 	$("#canvas").mousedown(function(e){lineMouse(e);});
-// 	$("#canvas").mouseup(function(e){lineMouse(e);}); 
-// });
-
-////////////////////////////////////////////////////////////////////:
-
-
 //LIGNE
 var mouse = {
     x: 0,
     y: 0
 };
 
-$("#line").click(function(){
-    $("#canvas").css('cursor','crosshair');
-    $("canvas").click(function(el){
-        if(mouse.x != 0 && mouse.y != 0){
-        	
+var lisDrawing = false;
+    function linehandleMouseDown(el){
+      if(mouse.x != 0 && mouse.y != 0){
+        var lisDrawing = false;
+          
             ctx.beginPath();
             ctx.moveTo(mouse.x, mouse.y);
             ctx.lineTo(el.offsetX, el.offsetY);
@@ -160,9 +112,17 @@ $("#line").click(function(){
             mouse.x = 0;
             mouse.y = 0;
         }else{
+          var lisDrawing = true;
             mouse.x = el.offsetX;
             mouse.y = el.offsetY;
+            $("#canvas").css('cursor','crosshair');
         }
+
+    }
+
+    $("#line").click(function(){
+    $("canvas").mousedown(function(el){
+      linehandleMouseDown(el);
     });
 });
 
@@ -181,19 +141,15 @@ $("#canvas").mousedown(function (ec) {
 function circlehandleMouseDown(ec) {
     cmouseX = parseInt(ec.clientX - offsetX);
     cmouseY = parseInt(ec.clientY - offsetY);
-    // $("#downlog").html("Down: " + mouseX + " / " + mouseY);
 
-    // Put your mousedown stuff here
     if (cisDrawing) {
         cisDrawing = false;
         ctx.strokeStyle = $("#color_picker").val();
-        ctx.beginPath();
         ctx.lineWidth = $("#epaisseurC").val();
+
+        ctx.beginPath();
         var dist = Math.sqrt((cmouseX - cstartX) * (cmouseX - cstartX) + (cmouseY - cstartY) * (cmouseY - cstartY));
         ctx.arc(cstartX, cstartY, dist, 0, 2 * Math.PI);
-        // console.log(((cmouseY-cstartY)/(cmouseX - cstartX)));
-        // console.log(dist);
-        // ctx.arc(mouseX,mouseY, 20 , 0, 2 * Math.PI);
         ctx.closePath();
         ctx.stroke(); 
         canvas.style.cursor = "default";
@@ -206,7 +162,7 @@ function circlehandleMouseDown(ec) {
 
 }
 
-//CIRCLE REMPLI
+//CERCLE REMPLI
 
 var crisDrawing = false;
 var crstartX;
@@ -220,14 +176,14 @@ $("#canvas").mousedown(function (ecr) {
 function RcirclehandleMouseDown(ecr) {
     crmouseX = parseInt(ecr.clientX - offsetX);
     crmouseY = parseInt(ecr.clientY - offsetY);
-    // Put your mousedown stuff here
+
     if (crisDrawing) {
         crisDrawing = false;
         ctx.beginPath();
         var dist = Math.sqrt((crmouseX - crstartX) * (crmouseX - crstartX) + (crmouseY - crstartY) * (crmouseY - crstartY));
         ctx.arc(crstartX, crstartY, dist, 0, 2 * Math.PI);
         ctx.closePath();
-        ctx.fillStyle = $("#color_picker").val();;
+        ctx.fillStyle = $("#color_picker").val();
         ctx.fill(); 
         canvas.style.cursor = "default";
     } else {
@@ -243,38 +199,38 @@ function RcirclehandleMouseDown(ecr) {
 
 //RECTANGLE VIDE
 
-var isDrawing = false;
-var startX;
-var startY;
+var rvisDrawing = false;
+var rvstartX;
+var rvstartY;
 
 $("#rectangle").click(function(){
-$("#canvas").mousedown(function (e) {
-    recthandleMouseDown(e);
+$("#canvas").mousedown(function (erv) {
+    recthandleMouseDown(erv);
 });
 });
-function recthandleMouseDown(e) {
-    mouseX = parseInt(e.clientX - offsetX);
-    mouseY = parseInt(e.clientY - offsetY);
-    // $("#downlog").html("Down: " + mouseX + " / " + mouseY);
+function recthandleMouseDown(erv) {
+    rvmouseX = parseInt(erv.clientX - offsetX);
+    rvmouseY = parseInt(erv.clientY - offsetY);
 
-    // Put your mousedown stuff here
-    if (isDrawing) {
-        isDrawing = false;
+    if (rvisDrawing) {
+        rvisDrawing = false;
         ctx.strokeStyle = $("#color_picker").val();
-        ctx.beginPath();
         ctx.lineWidth = $("#epaisseurRV").val();
-        ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
+
+        ctx.beginPath();
+        ctx.rect(rvstartX, rvstartY, rvmouseX - rvstartX, rvmouseY - rvstartY);
         ctx.closePath();
         ctx.stroke(); 
         canvas.style.cursor = "default";
     } else {
-        isDrawing = true;
-        startX = mouseX;
-        startY = mouseY;
+        rvisDrawing = true;
+        rvstartX = rvmouseX;
+        rvstartY = rvmouseY;
         $("#canvas").css('cursor','crosshair');
     }
 
 }
+
 //RECTANGLE REMPLI
 
 var risDrawing = false;
@@ -289,9 +245,7 @@ $("#canvas").mousedown(function (er) {
 function rectRhandleMouseDown(er) {
     rmouseX = parseInt(er.clientX - offsetX);
     rmouseY = parseInt(er.clientY - offsetY);
-    // $("#downlog").html("Down: " + mouseX + " / " + mouseY);
 
-    // Put your mousedown stuff here
     if (risDrawing) {
         risDrawing = false;
         ctx.fillStyle = $("#color_picker").val();
@@ -315,26 +269,16 @@ button.addEventListener('click', function (e) {
     var dataURL = canvas.toDataURL('image/png');
     button.href = dataURL;
 });
-//OUVRIR FICHIER
-$('#button').on('click', function() {
-    $('#file-input').trigger('click');
-});
 
 //CLEAR
 document.getElementById('clear').addEventListener('click', function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
       }, false);
 
-//BACK
-// document.getElementById('cancel').addEventListener('click', function(event) {
-
-//       event.preventDefault();
-//       // canvas.remove();
-
-//       // return false;
-
-//     });
-
+//OUVRIR FICHIER
+// $('#button').on('click', function() {
+//     $('#file-input').trigger('click');
+// });
 
 //COULEUR CANVAS
 // $("#color_pickerbg").click(function(){
